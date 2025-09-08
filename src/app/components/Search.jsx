aef01447a5c578d2fa8327422c119b210596348b
vachan-maker <vachan@tutanota.com>
2025-09-08 23:57:39 +0530

    Fixing up logic for conditional rendering and better error handling

--- src/app/components/Search.jsx
+++ src/app/components/Search.jsx
@@ -66,9 +66,9 @@ export default function Search() {
                     <ul>
                     {results.map((item) => {
                         return <li key={item.show.id} className="flex flex-row mb-4">
-                            {item.show.image?(<img src={item.show.image.medium} alt={item.show.name} className="mr-4 rounded" />):<img src="https://dummyimage.com/210x295/cccccc/000000&text=No+Image"/>}
+                            {item.show.image?(<img src={item.show.image.medium} alt={item.show.name} className="mr-4 rounded hover:shadow-2xl" />):<img src="https://dummyimage.com/210x295/cccccc/000000&text=No+Image"/>}
                             <div className="flex flex-col gap-2">
-                            <h2 className="text-2xl">{item.show.name}</h2>
+                            <a href={`/shows/${item.show.id}`} className="hover:underline underline-offset-4"><h2 className="text-2xl">{item.show.name}</h2></a>
                             <p><b>Premiered On: </b> {item.show.premiered}</p>
                             <p><b>Ended On: </b>{item.show.ended}</p>
                             {item.show.summary?(<p className="line-clamp-3 max-w-lg">{item.show.summary.replace(/<[^>]+>/g, "")}</p>):("")}
--- src/app/shows/[id]/page.js
+++ src/app/shows/[id]/page.js
@@ -9,10 +9,12 @@ export default function Shows({params}) {
         try {
             setLoading(true)
             const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
+            console.log(res)
             if(!res.ok) {
                 throw new Error("Error fetching data from API")
             }
             const data = await res.json()
+            console.log(data)
             setData(data)
             console.log(data)
 
@@ -28,6 +30,14 @@ export default function Shows({params}) {
     },[id])
     return (
         <>
+        {loading && <p>Loading Data..</p>}
+        {!data && <p>Details not found</p>}
+
+        {!loading && (<div className="flex flex-col max-w-8/12 mx-auto my-0 pt-8">
+        {/* z */}
+        <h1 className="text-4xl">{data.name}</h1>
+        <p>{data.summary.replace(/<[^>]+>/g, "")}</p>
+        </div>)}
         </>
     )
 }
