e68b3ed5b1d61c7e583f62020fc4502ac6e6e6b9
vachan-maker <vachan@tutanota.com>
2025-09-09 15:12:11 +0530

    handle error

--- src/app/components/Episodes.jsx
+++ src/app/components/Episodes.jsx
@@ -4,12 +4,23 @@ import EpisodeItem from "./EpisodeItem"
 export default function Episodes({season}) {
     const [data,setData] = useState([])
     async function fetchEpisodes() {
+        try {
         const res = await fetch(`https://api.tvmaze.com/seasons/${season}/episodes`)
+        if(!res.ok) {
+            throw new Error("Error fetching data from API")
+
+        }
         const data = await res.json()
         setData(data)
+        }
+        catch(error) {
+            console.error("Error")
+        }
     }
     useEffect(()=>{
+        if(season) {
         fetchEpisodes(season)
+        }
     },[season])
     return (
         <div className="flex flex-col">  
