515b97e273da2b2eee9b4641f42aadf46a43d73c
vachan-maker <vachan@tutanota.com>
2025-09-11 20:56:20 +0530

    Added search on keydown

--- src/app/components/Search.jsx
+++ src/app/components/Search.jsx
@@ -4,83 +4,10 @@ import { useEffect, useState } from "react"
 import SearchResults from "./SearchResults"
 import SearchBar from "./SearchBar"
 export default function Search() {
-    const [loading, setLoading] = useState(false)
-    const [results, setResults] = useState([])
     const [input, setInput] = useState("")
-    const [isActive,setIsActive] = useState(false)
-
-
-    const searchParams = useSearchParams()
-    const pathName = usePathname()
-    const { replace } = useRouter()
-    async function searchShows(query) {
-        try {
-            setLoading(true)
-            const res = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`)
-            if (!res.ok) {
-                throw new Error("Failed to fetch data!")
-            }
-            const data = await res.json()
-            return data
-        } catch (error) {
-            console.error(error)
-            return []
-        }
-        finally {
-            setLoading(false)
-        }
-    }
-    function updateSearchParams(query) {
-        const params = new URLSearchParams(searchParams)
-        if (query) {
-            params.set('query', query)
-        }
-        else {
-            params.delete('query')
-        }
-        replace(`${pathName}?${params.toString()}`);
-    }
-    async function handleSearch(query) {
-        updateSearchParams(input)
-        const data = await searchShows(query)
-        setResults(data)
-        console.log(data)
-
-    }
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            if (input.trim() !== "") {
-                handleSearch(input)
-            } else {
-                updateSearchParams("")
-                setResults([])
-            }
-        }, 1000)
-        
-        return () => clearTimeout(timeout)
-    }, [input])
-    useEffect(()=>async()=>{
-        const query = searchParams.get("query" || "")
-        if(query) {
-            const data = await searchShows(query)
-            setResults(data)
-        }
-        else {
-            setResults([])
-        }
-    },[searchParams])
     return (
         <>
             <SearchBar input={input} setInput={setInput}/>
-            {loading && <h1>Loading Data....</h1>}
-                {!loading && results.length > 0 && (
-                    <ul>
-                    {results.map((item) => {
-                        return <li key={item.show.id} className="flex flex-row mb-4"><SearchResults item={item}/>
-                            </li>
-                    })}
-                    </ul>
-                )}
         </>
     )
 }
--- src/app/components/SearchBar.jsx
+++ src/app/components/SearchBar.jsx
@@ -1,7 +1,23 @@
-export default function SearchBar({input,setInput}) {
+import { useState } from "react"
+import { useRouter } from "next/navigation"
+export default function SearchBar({}) {
+    const [input,setInput] = useState("")
+    const router = useRouter()
+    function handleSearch(input) {
+        if (input.trim() == "") return ;
+
+
+        router.push(`search?q=${encodeURIComponent(input)}`)
+    }
+    function handleKeyDown(event) {
+        if(event.key == 'Enter') {
+            handleSearch(input)
+        }
+    }
+
     return (
         <>
-        <input type="text" value={input} onChange={(e) => { setInput(e.target.value) }} className="border rounded-md px-2 py-2 w-2xl"/>
+        <input type="text" value={input} onChange={(e) => { setInput(e.target.value) }} className="border rounded-md px-2 py-2 w-2xl" onKeyDown={(e)=>handleKeyDown(e)}/>
         </>
     )
 }
