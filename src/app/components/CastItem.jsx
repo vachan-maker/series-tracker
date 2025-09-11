721b441c77fb264ce279a1019cc48f9960ef7f61
vachan-maker <vachan@tutanota.com>
2025-09-11 18:47:38 +0530

    Error handling for cast images

--- src/app/components/CastItem.jsx
+++ src/app/components/CastItem.jsx
@@ -1,9 +1,10 @@
 export default function CastItem({castData}) {
     return (
         <div className="flex flex-col items-center">
-            <img src={castData.person.image.medium?(
-                castData.person.image.medium
-            ):("https://dummyimage.com/210x295/cccccc/000000&text=No+Image")}/>
+            {castData.person.image?(castData.character.image.medium?(
+                <img src={castData.character.image.medium}/>
+            ):(castData.person.image.medium)
+            ):(<h1>IMage not found</h1>)}
             <p>{castData.person.name}</p>
             <p>{castData.character.name}</p>
         </div>
