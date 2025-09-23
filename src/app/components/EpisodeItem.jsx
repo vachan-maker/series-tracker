717dcf1dd29467e3fece5fec31e2b194f21cfd5d
vachan-maker <vachan@tutanota.com>
2025-09-23 10:08:06 +0530

    Making things more responsive

--- src/app/components/EpisodeItem.jsx
+++ src/app/components/EpisodeItem.jsx
@@ -1,6 +1,6 @@
 export default function EpisodeItem({episode}) {
     return (
-        <div className="flex flex-col py-4 my-4 w-9/12 border-2 border-slate-200 p-3 rounded">
+        <div className="flex flex-col py-4 my-4 w-full lg:w-9/12 border-2 border-slate-200 p-3 rounded">
           <h2 className="text-xl mb-2 font-bold">S{episode.season}.{episode.number} - {episode.name}</h2>
           <p>{episode.summary?(episode.summary.replace(/<[^>]+>/g, "")):("No description")}</p>
         </div>
